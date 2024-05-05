ef6b605809b124ae921253df84721c6ea1965085
Lucas Santos <lucas201273@gmail.com>
2024-05-05 15:23:23 -0300

    feat: add project architeture

--- lib/ui/screens/home/index.dart
+++ lib/ui/screens/home/index.dart
@@ -2,19 +2,18 @@ import 'package:despesas/entities/transaction/index.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
-  Home({super.key});
-
-  final _transctions = [
-    Transaction(
-        id: '1241', title: 'any_title', value: 39.10, date: DateTime.now()),
-    Transaction(
-        id: '1242', title: 'any_title2', value: 200.10, date: DateTime.now()),
-    Transaction(
-        id: '1243', title: 'any_title3', value: 300.10, date: DateTime.now())
-  ];
+  const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final List<Transaction> transactions = [
+      Transaction(
+          id: '1241', title: 'any_title', value: 39.10, date: DateTime.now()),
+      Transaction(
+          id: '1242', title: 'any_title2', value: 200.10, date: DateTime.now()),
+      Transaction(
+          id: '1243', title: 'any_title3', value: 300.10, date: DateTime.now())
+    ];
     return Scaffold(
         appBar: AppBar(
             backgroundColor: Colors.blue,
@@ -30,11 +29,11 @@ class Home extends StatelessWidget {
                 Icon(Icons.add, color: Colors.white)
               ],
             )),
-        body: const Column(
+        body: Column(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            SizedBox(
+            const SizedBox(
               width: double.infinity,
               child: Card(
                 color: Colors.blue,
@@ -42,12 +41,16 @@ class Home extends StatelessWidget {
                 child: Text('Gráfico'),
               ),
             ),
-            SizedBox(
+            const SizedBox(
                 width: double.infinity,
                 child: Card(
                   elevation: 5,
                   child: Text('Lista de Transações'),
-                ))
+                )),
+            Column(
+                children: transactions.map((tr) {
+              return Card(child: Text(tr.title));
+            }).toList())
           ],
         ));
   }
