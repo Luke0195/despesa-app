88c9985aa8e51c7291cb7fb816d3230b5d5d46a5
Lucas Santos <lucas201273@gmail.com>
2024-05-06 21:35:45 -0300

    feat: aplicando estilos no texto

--- lib/ui/screens/home/components/transaction_item.dart
+++ lib/ui/screens/home/components/transaction_item.dart
@@ -9,32 +9,15 @@ class TransactionItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ClipRRect(
-      borderRadius: BorderRadius.circular(5),
+      borderRadius: BorderRadius.circular(8),
       child: Container(
-          width: double.infinity,
-          decoration:
-              BoxDecoration(border: Border.all(color: Colors.grey, width: 2)),
-          child: Card(
-            child: Row(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                ClipRRect(
-                    borderRadius: const BorderRadius.all(Radius.circular(50)),
-                    child: Container(
-                        color: Colors.purple,
-                        width: 60,
-                        height: 60,
-                        alignment: Alignment.center,
-                        child: Text("R\$ ${transaction.value}"))),
-                Column(
-                  children: [
-                    Text(transaction.title),
-                    Text("${transaction.date}")
-                  ],
-                )
-              ],
-            ),
-          )),
+        margin: const EdgeInsets.symmetric(
+          horizontal: 15,
+          vertical: 10,
+        ),
+        decoration: BoxDecoration(
+            border: Border.all(color: Colors.grey.shade300, width: 1)),
+      ),
     );
   }
 }
